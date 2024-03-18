7b72ae3dad943e76bcaddfa21ec72c111ab440ad
natandreli <natalia.garcia9@udea.edu.co>
2024-03-18 08:39:07 -0500

    Created folders for controller, model and service

---
+++ src/main/java/com/udea/authorizationauthentication/AuthorizationAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.udea.authorizationauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/authorizationauthentication/AuthorizationAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.authorizationauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
