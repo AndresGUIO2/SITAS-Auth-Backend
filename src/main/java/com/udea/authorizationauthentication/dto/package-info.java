5f47bbe1ebe3ee0b90236c0ad3f2264e038efd8d
Héctor Andrés Güiza <110676601+AndresGUIO2@users.noreply.github.com>
2024-03-19 22:30:01 -0500

    Added package info for dto

---
+++ src/main/java/com/udea/authorizationauthentication/dto/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Provides the classes necessary for data transfer objects (DTOs)
+ * between different layers of the application.
+ *
+ * <p>DTOs are used to encapsulate data, and they provide a layer of abstraction
+ * which can help protect your application from changes. They are particularly
+ * useful for passing data between the service layer and controllers, and for
+ * structuring the data sent over the network.</p>
+ *
+ * <p>This package contains the {@link com.udea.authorizationauthentication.dto.PersonRegistrationDTO}
+ * which is responsible for carrying person registration data from clients to the server for processing.</p>
+ */
+package com.udea.authorizationauthentication.dto;
+
+
