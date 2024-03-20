ad81233487d410c884fab36142b58c29334ca825
natandreli <natalia.garcia9@udea.edu.co>
2024-03-19 23:11:42 -0500

    Add some service's documentation

--- src/main/java/com/udea/authorizationauthentication/service/package-info.java
+++ src/main/java/com/udea/authorizationauthentication/service/package-info.java
@@ -1,1 +1,15 @@
+/**
+ * This package contains classes related to authentication and authorization services.
+ * It provides functionalities for user registration, authentication, and authorization operations.
+ * Classes:
+ * <ul>
+ *     <li>{@link com.udea.authorizationauthentication.service.AuthenticationService}</li>
+ *     <li>{@link com.udea.authorizationauthentication.service.PersonService}</li>
+ * </ul>
+ *
+ * @author Natalia García
+ * @author Héctor Güiza
+ * @author Jeisson Barrantes
+ * @author Hellen Rubio
+ */
 package com.udea.authorizationauthentication.service;
