14085a2d3124653e1861c1a8bf93269ef0fc2a3b
natandreli <natalia.garcia9@udea.edu.co>
2024-03-19 22:28:00 -0500

    Add controller's documentation

--- src/main/java/com/udea/authorizationauthentication/controller/PersonController.java
+++ src/main/java/com/udea/authorizationauthentication/controller/PersonController.java
@@ -6,6 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * This class represents the controller for handling person-related HTTP requests.
+ * It maps incoming requests to methods for register a new person.
+ *
+ * @author Natalia García
+ * @author Héctor Güiza
+ * @author Jeisson Barrantes
+ * @author Hellen Rubio
+ */
 @RestController
 @RequestMapping("/public/api/auth")
 public class PersonController {
@@ -13,6 +22,12 @@ public class PersonController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    /**
+     * Handles HTTP POST requests to register a new person.
+     *
+     * @param registrationDto the DTO containing user registration data
+     * @return ResponseEntity containing the registered user entity
+     */
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody PersonRegistrationDTO registrationDto) {
         Person person = authenticationService.registerPerson(registrationDto);
