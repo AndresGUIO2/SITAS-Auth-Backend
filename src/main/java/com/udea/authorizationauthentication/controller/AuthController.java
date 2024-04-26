566e7fb1ce02d80a35a160636028b93ff94770e4
natandreli <natalia.garcia9@udea.edu.co>
2024-04-26 14:37:05 -0500

    Add swagger documentation

--- src/main/java/com/udea/authorizationauthentication/controller/AuthController.java
+++ src/main/java/com/udea/authorizationauthentication/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.udea.authorizationauthentication.config.security.JwtTokenProvider;
 import com.udea.authorizationauthentication.dto.JwtResponse;
 import com.udea.authorizationauthentication.dto.LoginRequest;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,9 +16,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "SIGN IN", description = "POST sign in person method")
+@Tag(name = "LOGIN", description = "POST login person method")
 @RestController
 @RequestMapping("/public/api/auth")
 public class AuthController {
@@ -32,12 +35,22 @@ public class AuthController {
 
     @Operation(summary = "Login a person", description = "Authenticate a user with provided credentials and generate a JWT token.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Successful authentication, JWT token generated"),
-            @ApiResponse(responseCode = "400", description = "Bad request, invalid input provided"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized, bad credentials provided")
+            @ApiResponse(responseCode = "200",
+                    description = "Successful authentication, JWT token generated",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = JwtResponse.class)
+                    )),
+            @ApiResponse(responseCode = "401",
+                    description = "Unauthorized, bad credentials provided",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            schema = @Schema(type = "string")
+                    ))
     })
     @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> authenticateUser(
+            @RequestBody LoginRequest loginRequest) {
         try {
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
--- src/main/java/com/udea/authorizationauthentication/dto/LoginRequest.java
+++ src/main/java/com/udea/authorizationauthentication/dto/LoginRequest.java
@@ -1,11 +1,14 @@
 package com.udea.authorizationauthentication.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class LoginRequest {
+    @NotNull
     private String username;
+    @NotNull
     private String password;
 }
